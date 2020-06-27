e6548321408e6b552c59b110894eee8433c261e4
TIAGO AMERICANO RODRIGUES <tiago.americano.03@gmail.com>
2020-06-27 20:06:32 -0300

    Limpei a estrutura e deixei tudo pronto para começar a apicação
    Criei um component Home e adicionei os arquivos de estilização

---
+++ web/src/components/Home/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Styles from './styles'
+// import { Container } from './styles';
+
+function Home() {
+  return (
+  <Styles>
+    Eai?
+
+  </Styles>
+  )
+}
+
+export default Home;
---
+++ web/src/styles/Global.js
@@ -0,0 +1,13 @@
+import {createGlobalStyle} from 'styled-components'
+
+export default createGlobalStyle`
+  body {
+      font-family: Arial, Helvetica, sans-serif;
+      font-size: 14px;
+      background: white;
+      text-rendering: optimizeLegibility;
+      -webkit-font-smoothing: antialiased;
+  }
+
+
+`
