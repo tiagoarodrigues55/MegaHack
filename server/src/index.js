9c40ff258e3b74d8e972ef769ab5aa43f6d89245
TIAGO AMERICANO RODRIGUES <tiago.americano.03@gmail.com>
2020-07-01 19:55:48 -0300

    limpei o repositório
    tirei toda a parte de conexão com o mongo e outras coisas desnecessárias...

--- server/src/index.js
+++ server/src/index.js
@@ -2,13 +2,11 @@ require('dotenv').config()
 
 const express = require('express')
 const morgan = require('morgan')
-const mongoose = require('mongoose')
 const path = require('path')
 const cors = require('cors')
 
 const app = express()
 
-mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
 
 app.use(cors())
 app.use(express.json())
--- server/src2/config/multer.js
+++ server/src2/config/multer.js
@@ -5,17 +5,17 @@ const multerS3 = require('multer-s3')
 const aws = require("aws-sdk")
 
 const storageTypes = {
-    local: multer.diskStorage({
-        destination: (req, file, cb) => {
-            cb(null, path.resolve(__dirname, '..', '..', 'tmp', 'uploads'))
-        },
-        filename: (req, file, cb) => {
-           
+    s3: multerS3({
+        s3: new aws.S3(),
+        bucket: 'uploadapipost',
+        contentType: multerS3.AUTO_CONTENT_TYPE,
+        acl: "public-read",
+        key: (req, file, cb) =>{
             crypto.randomBytes(5, (err, hash) => {
                 if(err) cb(err)
             
-                file.key = `${hash.toString('hex')}-${file.originalname}`
-                cb(null, file.key)
+                const fileName = `${hash.toString('hex')}-${file.originalname}`
+                cb(null, fileName)
             })
         }
     })
@@ -24,6 +24,6 @@ const storageTypes = {
 
 module.exports = {
 dest: path.resolve(__dirname, '..', '..', 'tmp', 'uploads'),
-storage: storageTypes[process.env.STORAGE_TYPE],
+storage: storageTypes[s3],
 
 }
