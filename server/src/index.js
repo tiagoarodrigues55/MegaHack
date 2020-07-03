c12091e3b73af3a0b144475bac7c224247f6b4b4
Natália Ramalho <natalia.ramalho.lopes@gmail.com>
2020-07-03 15:49:52 -0300

    Adicionando o banco de dados e rotas get, put, delete e post

--- server/src/index.js
+++ server/src/index.js
@@ -13,8 +13,10 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true}))
 app.use(morgan("dev"))
 app.use('/files', 
-  express.static(path.resolve(__dirname, "..", "tmp", "uploads")))
+express.static(path.resolve(__dirname, "..", "tmp", "uploads")))
 
 app.use(require("./routes"))
+
+
 
 app.listen(process.env.PORT || 3001)
