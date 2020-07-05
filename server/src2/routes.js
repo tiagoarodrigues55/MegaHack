00411d16b0dc775465327af57149573bcb30d2ff
TIAGO AMERICANO RODRIGUES <tiago.americano.03@gmail.com>
2020-07-05 00:36:56 -0300

    Alterei a requisição do multer de deploy
    Era single virou array, para enviar todos os arquivos da pasta

--- server/src2/routes.js
+++ server/src2/routes.js
@@ -2,7 +2,7 @@ const routes = require('express').Router()
 const multer = require('multer')
 const multerConfig = require('./config/multer')
 
-routes.post("/deploy", multer(multerConfig).single("file"), async (req, res) => {
+routes.post("/deploy", multer(multerConfig).array("file", 10), async (req, res) => {
    console.log(req.file)
    console.log(req)
    return
